d62dc27be6d0c5d19f27b4aefcf7ce2e5233b5a2
rohitsingh0466 <rohit0904singh@gmail.com>
2026-04-11 11:07:07 +0530

    Add Google Analytics tracking functions

---
+++ frontend/lib/gtag.ts
@@ -0,0 +1,27 @@
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!;
+
+// Log page views
+export const pageview = (url: string) => {
+  window.gtag('config', GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
+
+// Log custom events
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label?: string;
+  value?: number;
+}) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
