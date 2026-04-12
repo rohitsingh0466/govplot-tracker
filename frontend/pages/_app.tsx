bb33ee9605390abce6641cd5c738a76d54eca811
rohitsingh0466 <rohit0904singh@gmail.com>
2026-04-12 14:55:49 +0530

    Wrap App component with ThemeProvider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 import { AuthProvider } from "../lib/AuthContext";
+import { ThemeProvider } from "../context/ThemeContext";
 import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -25,40 +26,42 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [gaMeasurementId]);
 
   return (
-    <AuthProvider>
-      {gaMeasurementId && (
-        <>
+    <ThemeProvider>
+      <AuthProvider>
+        {gaMeasurementId && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${gaMeasurementId}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
+        {adsenseClient && (
           <Script
+            id="adsense-script"
+            async
             strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+            crossOrigin="anonymous"
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClient}`}
           />
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaMeasurementId}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </>
-      )}
-      {adsenseClient && (
-        <Script
-          id="adsense-script"
-          async
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClient}`}
-        />
-      )}
-      <Component {...pageProps} />
-      <Analytics />
-    </AuthProvider>
+        )}
+        <Component {...pageProps} />
+        <Analytics />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
