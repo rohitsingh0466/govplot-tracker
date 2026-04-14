2a799dcbf81b9c2a255e5d578406558ee750bd70
rohitsingh0466 <rohit0904singh@gmail.com>
2026-04-14 09:23:14 +0530

    Update urls.py

--- backend/routes/scm/urls.py
+++ backend/routes/scm/urls.py
@@ -26,7 +26,7 @@ from sqlalchemy.orm import Session
 import json
 
 from backend.models.database import get_db
-from backend.routes.admin import get_current_admin
+from backend.routes.admin_auth import get_current_admin
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
